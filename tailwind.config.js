866ce1b325322c36dcfb3dee130a1a7c41a52965
Naveja00 <164282765+Naveja00@users.noreply.github.com>
2026-05-06 07:17:44 -0500

    Wire GiftCurator to local Ollama and add no-preference tone

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        terracotta: '#C2622A',
+        gold: '#D7A24C',
+        cream: '#FAF5EF'
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+};
